3117e23b07fd0b0aaab9364fd3af4347a314c653
Jura L <jural@Juras-MBP.lan>
2020-10-25 11:57:27 +0200

    Add router and containers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Container, Nav, Button, NavDropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import Routes from './Routes';
 import './App.css';
+import DropdownItem from 'react-bootstrap/esm/DropdownItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App container'>
+      <Navbar expand='sm' variant="light">
+        <Container>
+          <Navbar.Brand>
+            <Link to='/'>
+              Solution
+            </Link>
+          </Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to='/'>
+              Your Name
+            </Nav.Link>
+            <NavDropdown title='Services'>
+              <DropdownItem>Sport</DropdownItem>
+              <DropdownItem>Adventure</DropdownItem>
+              <DropdownItem>Local products</DropdownItem>
+            </NavDropdown>
+            <Nav.Link as={Link} to='/'>
+              Benefits
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <LinkContainer to='/signup' className='mr-sm-2'>
+              <Button>Signup</Button>
+            </LinkContainer>
+            <LinkContainer to='/login'>
+              <Button variant='outline-primary'>Login</Button>
+            </LinkContainer>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes />
     </div>
   );
 }
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+export default function Home () {
+  return (
+    <div className='Home'>
+      <div className='lander'>
+        <h1>Solutions</h1>
+        <p>Follow the roots of your culture</p>
+      </div>
+    </div>
+  )
+};
